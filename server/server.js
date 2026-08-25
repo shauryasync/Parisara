77b57c16355810fa71c0aaf474fa9d8b636bf053
Shaurya <edshaurya@gmail.com>
2026-08-26 00:02:38 +0530

    feat: connect MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +13,14 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log("Server running...");
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log("Server running...");
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
